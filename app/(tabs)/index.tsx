9eaca3224ed122bf3f95b4c21e0f2e033565b9d8
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-09 12:21:11 +0530

    done the button add

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,25 +1,49 @@
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
 import { useTheme } from "@react-navigation/native";
 import Colors from "@/constants/Colors";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { useRouter } from "expo-router";
 
 const index = () => {
   const theme = useTheme();
   const isDark = theme.dark ? true : false;
+  const router = useRouter();
   return (
     <SafeAreaView style={styles.mainview}>
-      <View style={styles.loginInput}>
+      {/* this is the bannerpage */}
+      <View style={styles.tileBanner}>
+        <Text
+          style={[
+            styles.title,
+            { color: isDark ? Colors.dark.text : Colors.light.text },
+          ]}
+        >
+          Password Manager
+        </Text>
         <Text
           style={[
             styles.text,
             { color: isDark ? Colors.dark.text : Colors.light.text },
           ]}
         >
-          SignUp Page
+          Manage and secure all your passwords in one place
         </Text>
-        <TextInput placeholder="Enter you username..." />
+        <TouchableOpacity onPress={() => router.push("/add")}
+          >
+          Add Password
+        </TouchableOpacity>
       </View>
+
+      <View style={styles.mainview}>another div</View>
+      <View style={styles.mainview}>another div</View>
     </SafeAreaView>
   );
 };
@@ -36,7 +60,14 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 500,
   },
-  loginInput: {
+  tileBanner: {
     gap: 10,
+    padding: 20,
+    flex: 1,
+    flexDirection: "column",
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 500,
   },
 });
