d6d59c1b2517edc1863e9ab9912c4aa1fce54938
Aditya Rawat <adityarawatnew2487@gmail.com>
2025-08-09 13:44:59 +0530

    time to add the logo

--- app/signup.tsx
+++ app/signup.tsx
@@ -2,18 +2,24 @@ import {
   SafeAreaView,
   StyleSheet,
   Text,
+  TextInput,
   useColorScheme,
   View,
 } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import Colors from "@/constants/Colors";
 
 const signup = () => {
   const theme = useColorScheme();
   const isDark = theme === "dark";
-
+  const [formData, setFormdata] = useState({
+    username: "",
+    password: "",
+  });
+  console.log(formData);
   return (
     <SafeAreaView style={styles.maindiv}>
+      {/* this is the tile of the sighnhp page */}
       <Text
         style={[
           styles.titleText,
@@ -22,6 +28,14 @@ const signup = () => {
       >
         SecuraPass Password Manager
       </Text>
+      <TextInput
+        placeholder="Enter your username..."
+        placeholderTextColor={isDark ? "#9CA3AF" : "#6B7280"}
+        style={[
+          styles.inputfield,
+          { color: isDark ? Colors.dark.text : Colors.light.text },
+        ]}
+      />
     </SafeAreaView>
   );
 };
@@ -31,11 +45,21 @@ export default signup;
 const styles = StyleSheet.create({
   maindiv: {
     flex: 1,
+    gap: 10,
     justifyContent: "center",
     alignItems: "center",
   },
   titleText: {
-    fontSize: 10,
+    fontSize: 25,
     fontWeight: "500",
+  },
+  inputfield: {
+    borderWidth: 1,
+    borderColor: "#fff",
+    borderRadius: 8,
+    paddingHorizontal: 12,
+    paddingVertical: 10,
+    width: "80%",
+    fontSize: 16,
   },
 });
